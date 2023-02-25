53687876a648bde7c991f76ef16ee974393a1b73
Maha Abdelnabi <mahaomar@gmail.com>
2023-02-25 00:15:10 -0500

    adding questions

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+
+// title of  project and  Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 const questions = [
   // Project name
   {
     type: "input",
     name: "title",
-    message: "What is the title of the project? (Required)",
-  
+    message: "What is the title of the project?",
   },
   // Description of the project
   {
     type: "input",
     name: "description",
-    message: "Provide a description of the project (Required)",
-
+    message: "What is the description of your project?",
   },
-]
+  // Installation Instructions
+  {
+    type: "input",
+    name: "installation",
+    message: "How do you install your project?",
+  },
+  // Usage Information
+  {
+    type: "input",
+    name: "usage",
+    message: "How do you use this project? ",
+  },
+  // Contribution
+  {
+    type: "input",
+    name: "contribution",
+    message:
+      "How should other developers contribute to this project?",
+  },
+  // Test Instructions
+  {
+    type: "input",
+    name: "testing",
+    message: "How do you test this project?",
+  }
+];
