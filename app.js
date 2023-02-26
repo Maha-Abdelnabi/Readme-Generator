91f6fde9846295366c5376fa74d96c74cd01fe6a
Maha Abdelnabi <mahaomar@gmail.com>
2023-02-26 17:21:47 -0500

    add readme template

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./generateMarkdown");
 
 
 // title of  project and  Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
@@ -14,13 +15,13 @@ const questions = [
   {
     type: "input",
     name: "description",
-    message: "What is the description of your project?",
+    message: "What is the description of the project?",
   },
   // Installation Instructions
   {
     type: "input",
     name: "installation",
-    message: "How do you install your project?",
+    message: "How do you install the project?",
   },
   // Usage Information
   {
@@ -32,13 +33,34 @@ const questions = [
   {
     type: "input",
     name: "contribution",
-    message:
-      "How should other developers contribute to this project?",
+    message: "How should other developers contribute to this project?",
   },
   // Test Instructions
   {
     type: "input",
     name: "testing",
     message: "How do you test this project?",
-  }
+  },
+  // License Options
+  {
+    type: "checkbox",
+    name: "licensing",
+    message: "Choose a license for your project.",
+    choices: [
+      "Apache",
+      "MIT",
+      "Mozilla-Public",
+      "GNU-General-Public",
+      "Common-Development-and Distribution",
+      "None",
+    ]
+    
+  },
 ];
+
+//Create a function to write README file
+function writeToFile(fileName, data) {
+    
+}
+
+//Create a function to initialize app
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,33 @@
 function generateMarkdown(data) {
   return `# ${data.title}
+ ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Additional Info](#additional-info)
 
+
+  ## Description:
+  ${data.description}
+
+  ## Installation:
+  ${data.installation}
+
+  ## Usage:
+  ${data.usage}
+
+  ## License:
+  ${data.licensing}
+
+  ## Contribution:
+  ${data.contribution}
+
+  ## Testing:
+  ${data.testing}
+
+  
 `;
 }
 
